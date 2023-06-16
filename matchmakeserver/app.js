cc1c46f5a752efb8e104b8772f62e4e302a8c525
choe jong jin <choejj373@gmail.com>
2023-06-17 08:52:03 +0900

    make app.js

--- matchmakeserver/app.js
+++ matchmakeserver/app.js
@@ -1,1 +1,19 @@
 "use strict"
+
+const express = require("express");
+
+const app = express();
+
+const server = app.listen( 3001,()=>{
+    console.log("서버 시작");
+} );
+
+const io = SocketIO( server, {path: "socket.io" } );
+
+io.on("connection", function( socket ){
+    console.log( socket.id, " connected...");
+
+    socket.on("disconnect", (data)=>{
+        console.log( socket.id, " disconnected");
+    })
+});
